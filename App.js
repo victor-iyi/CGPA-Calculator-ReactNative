6e7556effdc6242f5a2448b94e60014c83b0e2e9
victor-iyiola <javafolabi@gmail.com>
2017-07-17 22:12:45 +0100

    Experimental

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { AppRegistry, View, Text } from 'react-native'
-import HomeScreen from './src/components/home-screen'
+import AppNavigator from './app-nav'
 import { styles } from './styles/index'
 
 export default class App extends Component {
@@ -8,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <HomeScreen />
+      
       </View>
     )
   }
