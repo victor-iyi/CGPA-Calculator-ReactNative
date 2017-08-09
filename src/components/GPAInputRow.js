40e7fb3081d398d64aad103252ee7e92789190f1
victor-iyiola <javafolabi@gmail.com>
2017-08-09 14:54:34 +0100

    Not working joorrr!

--- src/components/GPAInputRow.js
+++ src/components/GPAInputRow.js
@@ -9,17 +9,17 @@ export const GPAInputRow = (props) => {
     <View style={styles.gparow}>
       <Text style={styles.courseLabel}> {props.course} </Text>
       <Dropdown
-        style={{ height: 20, width: 200}}
-        values={[ '--Choose--', 'one', 2, 3.5, { four: 4 }, [ 5, 6, 7 ], false ]}
+        style={{ height: 20, width: 200 }}
+        values={['--Choose--', 'one', 2, 3.5, { four: 4 }, [5, 6, 7], false]}
         selected={1} onChange={(data) => { console.log(data); }} />
       <UIStepper
-        onValueChange={props.onUnitChange}
-        displayValue={true}
         minimumValue={0}
-        overrideTintColor={true}
+        displayValue={true}
         tintColor="#0a0a0a"
-        borderColor="#0a0a0a"
         textColor="#0a0a0a"
+        borderColor="#0a0a0a"
+        overrideTintColor={true}
+        onValueChange={props.onUnitChange}
       />
     </View>
   );
