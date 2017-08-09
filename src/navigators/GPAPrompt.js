8719fc7514177b8cf0dcec2b78ceeff07acf30a4
victor-iyiola <javafolabi@gmail.com>
2017-08-10 00:34:52 +0100

    Formatter

--- src/navigators/GPAPrompt.js
+++ src/navigators/GPAPrompt.js
@@ -5,58 +5,62 @@ import { styles } from '../styles';
 
 class GPAPrompt extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor( props ) {
+    super( props );
 
-    this.state = { result: '0' };
+    this.state = {
+      result: '0'
+    };
     // bindings
-    this._numButton = this._numButton.bind(this);
-    this._controlButton = this._controlButton.bind(this);
+    this._numButton = this._numButton.bind( this );
+    this._controlButton = this._controlButton.bind( this );
   }
 
-  static navigationOptions = ({ navigation }) => ({
-    title: 'GPA',
-  });
+  static navigationOptions = ({ navigation }) => ({ title: 'GPA' });
 
-  clearResult() {
+  clearResult( ) {
     if ( this.state.result > 1 ) {
       if ( this.state.result.length === 1 )
-        this.setState( { result: '0' });
+        this.setState({ result: '0' });
       else
         this.setState(prevState => {
-          return { result: prevState.result.slice(0, -1) }
+          return {
+            result: prevState.result.slice( 0, -1 )
+          }
         });
-    } else
-        this.setState({ result: '0' });
-  }
+      }
+    else
+      this.setState({ result: '0' });
+    }
 
-  _numButton(title) {
+  _numButton( title ) {
     if ( this.state.result.length < 3 )
-      this.setState((prevState) => {
-        return { result: prevState.result === '0' ? `${title}` : `${prevState.result}${title}`};
+      this.setState(( prevState ) => {
+        return {
+          result: prevState.result === '0'
+            ? `${ title }`
+            : `${ prevState.result }${ title }`
+        };
       });
-  }
+    }
 
-  _controlButton(action) {
-    if ( action === 'clear' ) this.clearResult();
+  _controlButton( action ) {
+    if ( action === 'clear' )
+      this.clearResult( );
     else {
-      const result = parseInt(this.state.result)
+      const result = parseInt( this.state.result )
       if ( result > 0 && result <= 12 ) {
         const { navigate } = this.props.navigation;
         navigate('CalculateGPA', { semesters: result });
-      } 
-      else Alert.alert('Number of semesters out of range.\nMin is 1 & Max is 12');
+      } else
+        Alert.alert( 'Number of semesters out of range.\nMin is 1 & Max is 12' );
+      }
     }
-  }
 
-  render() {
+  render( ) {
     return (
       <View style={styles.container}>
-        <NumberButtons 
-          result={this.state.result}
-          prompt="Enter number of semesters completed & press go!"
-          controlButton={this._controlButton}
-          numButton={this._numButton} />
+        <NumberButtons result={this.state.result} prompt="Enter number of semesters completed & press go!" controlButton={this._controlButton} numButton={this._numButton}/>
       </View>
     );
   }
