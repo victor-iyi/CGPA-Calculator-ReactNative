134ad38f8a4756dcf6e2708275ab62b83e8850f0
victor-iyiola <javafolabi@gmail.com>
2017-08-15 21:15:45 +0100

    Reformatting

--- src/navigators/CalculateCGPA.js
+++ src/navigators/CalculateCGPA.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
-import { View, ScrollView, Text } from 'react-native';
-import { Button } from 'react-native-elements';
-import { LevelCard } from '../components';
-import { styles } from '../styles';
+import React, {
+  Component
+} from 'react';
+import {
+  View,
+  ScrollView,
+  Text
+} from 'react-native';
+import {
+  Button
+} from 'react-native-elements';
+import {
+  LevelCard
+} from '../components';
+import {
+  styles
+} from '../styles';
 
 /**
  *************************************
@@ -24,11 +36,13 @@ class CalculateCGPA extends Component {
     this._updateGPA = this._updateGPA.bind(this);
     this._calculate = this._calculate.bind(this);
   }
-  
-  static navigationOptions = ({ navigation }) => ({
-    title: `Calculate CGPA for ${navigation.state.params.semesters} semesters`, //`$ {navigation.state.params.var}`
-    // headerRight: <Button title="Info" onPress={() => Alert.alert('Info nav clicked!') } />,
-  });
+
+  static navigationOptions = ({
+    navigation
+  }) => ({
+      title: `Calculate CGPA for ${navigation.state.params.semesters} semesters`, //`$ {navigation.state.params.var}`
+      // headerRight: <Button title="Info" onPress={() => Alert.alert('Info nav clicked!') } />,
+    });
 
 
   _updateGPA (index, gpa) {
@@ -46,22 +60,35 @@ class CalculateCGPA extends Component {
     for (let i = 0; i < sessions; i++) {
       const level = `${i + 1}00`,
         semesters = i + 1 === sessions && this.params.semesters % 2 !== 0 ? 1 : 2;
-      views.push(<LevelCard key={i} semesters={semesters} level={level} onChangeText={(sem, value) => {
-        console.log('i', i, 'sem', sem);
-        this._updateGPA(index, value);
-      }} />);
+      views.push(< LevelCard key={
+        i
+      }
+        semesters={
+          semesters
+        }
+        level={
+          level
+        }
+        onChangeText={
+          (sem, value) => {
+            console.log('i', i, 'sem', sem);
+            this._updateGPA(index, value);
+          }
+        }
+      />);
     }
     return views;
   }
 
   render () {
-    return (
-      <ScrollView style={styles.container}>
-        <View style={styles.calcCGPAContainer}>
-          {this.renderLevel()}
-          <Button raised icon={{ name: 'calculator', type: 'font-awesome' }} style={styles.calcButton} title="Calculate" onPress={this._calculate} />
-        </View>
-      </ScrollView>
+    return (<ScrollView style={styles.container}>
+      <View style={styles.calcCGPAContainer} >
+        this.renderLevel() }
+         <Button raised icon={{ name: 'calculator', type: 'font-awesome' }} style={styles.calcButton}
+          title="Calculate" onPress={this._calculate}
+        />
+      </View>
+    </ScrollView>
     );
   }
 
