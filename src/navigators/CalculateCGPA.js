b5c3b1accb557f9948c175019a9dc37f9365d486
victor-iyiola <javafolabi@gmail.com>
2017-09-03 13:52:56 +0100

    CalculateCGPA formatting

--- src/navigators/CalculateCGPA.js
+++ src/navigators/CalculateCGPA.js
@@ -10,60 +10,82 @@ import { styles } from '../styles';
  *************************************
  */
 class CalculateCGPA extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
     // component state
     this.state = {};
     // navigation params
     this.params = this.props.navigation.state.params;
     this.gpas = [];
-    for (let i = 0; i < this.params.semesters; i++)
-      this.gpas.push(0.00);
+    for (let i = 0; i < this.params.semesters; i++) this.gpas.push(0.0);
     // bindings
     this._updateGPA = this._updateGPA.bind(this);
     this._calculate = this._calculate.bind(this);
   }
 
+  /* +-------------------------------------------------------------------------------------
+   * | navigationOptions
+   * +------------------------------------------------------------------------------------- */
   static navigationOptions = ({ navigation }) => ({
-    title: `Calculate CGPA for ${navigation.state.params.semesters} semesters`, //`$ {navigation.state.params.var}`
+    title: `Calculate CGPA for ${navigation.state.params.semesters} semesters` //`$ {navigation.state.params.var}`
     // headerRight: <Button title="Info" onPress={() => Alert.alert('Info nav clicked!') } />,
   });
 
-  _updateGPA (index, gpa) {
+  /* +-------------------------------------------------------------------------------------
+   * | update GPA
+   * +------------------------------------------------------------------------------------- */
+  _updateGPA(index, gpa) {
     // TODO: update GPA list
     console.log(`index = ${index} -> gpa = ${gpa}`);
   }
 
-  _calculate () {
+  /* +-------------------------------------------------------------------------------------
+   * | calculate CGPA
+   * +------------------------------------------------------------------------------------- */
+  _calculate() {
     // calculates CGPA
   }
 
-  renderLevel () {
-    const views = []
+  /* +-------------------------------------------------------------------------------------
+   * | redner level & semesters
+   * +------------------------------------------------------------------------------------- */
+  renderLevel() {
+    const views = [];
     const sessions = Math.ceil(this.params.semesters / 2);
     for (let i = 0; i < sessions; i++) {
       const level = `${i + 1}00`,
         semesters = i + 1 === sessions && this.params.semesters % 2 !== 0 ? 1 : 2;
-      views.push(<LevelCard key={i} semesters={semesters} level={level} onChangeText={(sem, value) => {
-        console.log('i', i, 'sem', sem);
-        this._updateGPA(index, value);
-      }} />);
+      views.push(
+        <LevelCard
+          key={i}
+          semesters={semesters}
+          level={level}
+          onChangeText={(sem, value) => {
+            console.log('i', i, 'sem', sem);
+            this._updateGPA(index, value);
+          }}
+        />
+      );
     }
     return views;
   }
 
-  render () {
-    return (<ScrollView style={styles.container}>
-      <View style={styles.calcCGPAContainer}>
-        {this.renderLevel()}
-        <Button raised icon={{ name: 'calculator', type: 'font-awesome' }} style={styles.calcButton}
-          title="Calculate" onPress={this._calculate} />
-      </View>
-    </ScrollView>
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.calcCGPAContainer}>
+          {this.renderLevel()}
+          <Button
+            raised
+            icon={{ name: 'calculator', type: 'font-awesome' }}
+            style={styles.calcButton}
+            title="Calculate"
+            onPress={this._calculate}
+          />
+        </View>
+      </ScrollView>
     );
   }
-
 }
 
 export default CalculateCGPA;
